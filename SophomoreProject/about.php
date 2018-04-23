0cb80802df528cd2846f86f5e0efa06937dbb8dd
willwat <willwatson220@gmail.com>
2018-04-23 00:51:50 -0400

    Updated about page

--- SophomoreProject/about.php
+++ SophomoreProject/about.php
@@ -11,6 +11,18 @@ require('templates/header.html');
 	You may select to input your image through a file upload or by URL.<br>
 	Then the image is sent to the Watson AI which gives a response that is given to you.<br>
 </p>
+
+<h3>Steps Taken to Use IBM Watson Visual Recognition </h3>
+<p>
+	<ol>
+		<li>Setup an instance of IBM Watson Visual Recognition</li>
+		<li>Find sample CLI curl commands to get a feel for using IBM Watson</li>
+		<li>Research curl commands for your chosen programming language</li>
+		<li>Practice using the commands with your programming language</li>
+		<li>Create functions parameterizing the commands</li>
+	</ol>
+</p>
+
 <?php
 require('templates/footer.html');
 ?>
